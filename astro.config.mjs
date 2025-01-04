e0a9e855ed55476758f3338698e66f961ff17e6b
guilhermefgs <guilhermefgs@proton.me>
2025-01-04 19:52:57 -0300

    add: remark and rehype packages to enable latex math

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import rehypeKatex from 'rehype-katex'; // relevant
+import remarkMath from 'remark-math';   // relevant
+
 export default defineConfig({
   site: "https://guilhermefgs.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath], // relevant
+      rehypePlugins: [rehypeKatex] // relevant
+    }), 
+    sitemap(), 
+    tailwind(),
+  ],
 });
