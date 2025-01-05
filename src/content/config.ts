eb7478a28035d98138dca9d32fafa36ebe39caa4
guilhermefgs <guilhermefgs@proton.me>
2025-01-04 21:53:06 -0300

    rm: demo URL

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,7 +27,6 @@ const projects = defineCollection({
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
     repoURL: z.string().optional()
   }),
 });
